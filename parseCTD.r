f66f8f00b031c62285290fdd66d3d5099c85c7a4
iman-rezaeian <33256403+iman-rezaeian@users.noreply.github.com>
2017-11-09 08:02:36 -0500

    added new parser for CTD data

---
+++ parseCTD.r
@@ -0,0 +1,8 @@
+parseCTD <- function()
+{
+  CTD = read.csv("CTD_chem_gene_ixns.csv",stringsAsFactors = FALSE)
+  H=CTD[which(CTD$OrganismID==9606 | is.na(CTD$OrganismID)),]
+  DrugGene= H[,c("ChemicalName","GeneSymbol")]
+  DrugGene=unique(DrugGene)
+  save(DrugGene,file = "DrugGene.RData")
+}
